fd2d18b96ca1664794257c0c6e5f8b1545372035
Marcin Deptuła <marcin.deptula@live.com>
2014-05-18 23:00:21 +0200

    Library implementation

---
+++ NLog.FSharp/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+﻿namespace NLog.FSharp
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("NLog.FSharp")>]
+[<assembly: AssemblyDescription("F# for NLog helper")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("NLog.FSharp")>]
+[<assembly: AssemblyCopyright("Copyright Marcin Deptuła 2014")>]
+[<assembly: AssemblyTrademark("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("69C41A83-5BCA-440F-A372-BD424DE2AB3E")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do ()
---
+++ NLog.FSharp/Logger.fs
@@ -0,0 +1,37 @@
+﻿namespace NLog.FSharp
+
+type ILogger =
+    abstract Trace: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract Info: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract Warn: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract Error: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract Fatal: fmt: Printf.StringFormat<'a, unit> -> 'a
+
+type Logger(logger: NLog.Logger ) =
+    let logger = logger
+
+    new() = 
+        let callerType = 
+            System.Diagnostics.StackTrace(1, false)
+                .GetFrames().[0]
+                .GetMethod()
+                .DeclaringType
+        Logger(NLog.LogManager.GetLogger(callerType.Name))
+    
+    member __.Trace fmt =
+        Printf.kprintf (fun s -> logger.Trace(s)) fmt
+    member __.Info fmt = 
+        Printf.kprintf (fun s -> logger.Info(s)) fmt
+    member __.Warn fmt =
+        Printf.kprintf (fun s -> logger.Warn(s)) fmt
+    member __.Error fmt = 
+        Printf.kprintf (fun s -> logger.Error(s)) fmt
+    member __.Fatal fmt =
+        Printf.kprintf (fun s -> logger.Fatal(s)) fmt
+
+    interface ILogger with 
+        member __.Trace fmt = __.Trace fmt
+        member __.Info fmt = __.Info fmt
+        member __.Warn fmt = __.Warn fmt
+        member __.Error fmt = __.Error fmt
+        member __.Fatal fmt = __.Fatal fmt

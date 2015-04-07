97c401edb6e0206e9b54e6245f55ec9e50fbcb04
Tom Rathbone <tom.rathbone@starlizard.com>
2015-04-07 10:20:21 +0100

    Add support for WarnException, ErrorException and FatalException

--- NLog.FSharp/Logger.fs
+++ NLog.FSharp/Logger.fs
@@ -1,12 +1,17 @@
 ﻿namespace NLog.FSharp
 
+open System;
+
 type ILogger =
     abstract Trace: fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Debug: fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Info: fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Warn: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract WarnException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Error: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract ErrorException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Fatal: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract FatalException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
 
 type Logger(logger: NLog.Logger ) =
     let logger = logger
@@ -27,16 +32,24 @@ type Logger(logger: NLog.Logger ) =
         Printf.kprintf (fun s -> logger.Info(s)) fmt
     member __.Warn fmt =
         Printf.kprintf (fun s -> logger.Warn(s)) fmt
+    member __.WarnException (e:Exception) fmt =
+        Printf.kprintf (fun s -> logger.WarnException(s, e)) fmt
     member __.Error fmt = 
         Printf.kprintf (fun s -> logger.Error(s)) fmt
+    member __.ErrorException (e:Exception) fmt =
+        Printf.kprintf (fun s -> logger.ErrorException(s, e)) fmt
     member __.Fatal fmt =
         Printf.kprintf (fun s -> logger.Fatal(s)) fmt
-    
+    member __.FatalException (e:Exception) fmt =
+        Printf.kprintf (fun s -> logger.FatalException(s, e)) fmt
 
     interface ILogger with 
         member __.Trace fmt = __.Trace fmt
         member __.Debug fmt = __.Debug fmt
         member __.Info fmt = __.Info fmt
         member __.Warn fmt = __.Warn fmt
+        member __.WarnException e fmt = __.WarnException e fmt
         member __.Error fmt = __.Error fmt
+        member __.ErrorException e fmt = __.ErrorException e fmt
         member __.Fatal fmt = __.Fatal fmt
+        member __.FatalException e fmt = __.FatalException e fmt

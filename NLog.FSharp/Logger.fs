b1cadd05d991721e132f542744f9e5a5c880262f
Tom Rathbone <tom.rathbone@starlizard.com>
2015-04-07 13:43:38 +0100

    add TraceException, DebugException and InfoException
    also removed calls to deprecated NLOG methods and used overloads instead

--- NLog.FSharp/Logger.fs
+++ NLog.FSharp/Logger.fs
@@ -4,8 +4,11 @@ open System;
 
 type ILogger =
     abstract Trace: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract TraceException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Debug: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract DebugException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Info: fmt: Printf.StringFormat<'a, unit> -> 'a
+    abstract InfoException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Warn: fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract WarnException: e: Exception -> fmt: Printf.StringFormat<'a, unit> -> 'a
     abstract Error: fmt: Printf.StringFormat<'a, unit> -> 'a
@@ -26,27 +29,36 @@ type Logger(logger: NLog.Logger ) =
     
     member __.Trace fmt =
         Printf.kprintf (fun s -> logger.Trace(s)) fmt
+    member __.TraceException (e:Exception) fmt =
+        Printf.kprintf (fun s -> logger.Trace(s, e)) fmt
     member __.Debug fmt = 
         Printf.kprintf (fun s -> logger.Debug(s)) fmt
+    member __.DebugException (e:Exception) fmt =
+        Printf.kprintf (fun s -> logger.Debug(s, e)) fmt
     member __.Info fmt = 
         Printf.kprintf (fun s -> logger.Info(s)) fmt
+    member __.InfoException (e:Exception) fmt =
+        Printf.kprintf (fun s -> logger.Info(s, e)) fmt
     member __.Warn fmt =
         Printf.kprintf (fun s -> logger.Warn(s)) fmt
     member __.WarnException (e:Exception) fmt =
-        Printf.kprintf (fun s -> logger.WarnException(s, e)) fmt
+        Printf.kprintf (fun s -> logger.Warn(s, e)) fmt
     member __.Error fmt = 
         Printf.kprintf (fun s -> logger.Error(s)) fmt
     member __.ErrorException (e:Exception) fmt =
-        Printf.kprintf (fun s -> logger.ErrorException(s, e)) fmt
+        Printf.kprintf (fun s -> logger.Error(s, e)) fmt
     member __.Fatal fmt =
         Printf.kprintf (fun s -> logger.Fatal(s)) fmt
     member __.FatalException (e:Exception) fmt =
-        Printf.kprintf (fun s -> logger.FatalException(s, e)) fmt
+        Printf.kprintf (fun s -> logger.Fatal(s, e)) fmt
 
     interface ILogger with 
         member __.Trace fmt = __.Trace fmt
+        member __.TraceException e fmt = __.TraceException e fmt
         member __.Debug fmt = __.Debug fmt
+        member __.DebugException e fmt = __.DebugException e fmt
         member __.Info fmt = __.Info fmt
+        member __.InfoException e fmt = __.InfoException e fmt
         member __.Warn fmt = __.Warn fmt
         member __.WarnException e fmt = __.WarnException e fmt
         member __.Error fmt = __.Error fmt

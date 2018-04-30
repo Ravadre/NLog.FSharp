6aa72d799b792cc7db247f7925eeb6371b17f8ba
Marcin Deptuła <marcin.deptula@live.com>
2018-04-30 22:56:47 +0200

    Updated NLog version to 4.5.3

--- NLog.FSharp/AssemblyInfo.fs
+++ NLog.FSharp/AssemblyInfo.fs
@@ -9,14 +9,14 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("NLog.FSharp")>]
-[<assembly: AssemblyCopyright("Copyright Marcin Deptuła 2014")>]
+[<assembly: AssemblyCopyright("Copyright Marcin Deptuła 2014-2018")>]
 [<assembly: AssemblyTrademark("")>]
 
 [<assembly: ComVisible(false)>]
 
 [<assembly: Guid("69C41A83-5BCA-440F-A372-BD424DE2AB3E")>]
 
-[<assembly: AssemblyVersion("4.4.12")>]
-[<assembly: AssemblyFileVersion("4.4.12")>]
+[<assembly: AssemblyVersion("4.5.3")>]
+[<assembly: AssemblyFileVersion("4.5.3")>]
 
 do ()
